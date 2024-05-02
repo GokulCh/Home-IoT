a55f67a02ae5da37b06d08b9a970c37a7bb96c8f
Gokul Chaluvadi <109551554+GokulCh@users.noreply.github.com>
2024-05-02 17:10:11 -0400

    TEST

--- Arduino/sensor/sensor.ino
+++ Arduino/sensor/sensor.ino
@@ -3,14 +3,14 @@
 #include <ArduinoJson.h>
 
 // Constants
-const int SENSOR_PIN = 5;
 const char* SSID = "rpi";
 const char* WIFI_PASSWORD = "somepassword";
 const char* MQTT_SERVER = "10.42.0.1";
-const String SENSOR_NAME = "house_1";
-const String SENSOR_TYPE = "door_1";
+const String SENSOR_NAME = F("house_1");
+const String SENSOR_TYPE = F("door_1");
 const uint16_t MQTT_PORT = 1883;
-#define LED_PIN 2
+const int SENSOR_PIN = 5;
+const int LED_PIN = 2;
 
 // Global variables
 int previousSensorState = HIGH;
@@ -22,7 +22,7 @@ String macAddress;
 void connectWiFi();
 void connectMQTTServer();
 void mqttCallback(char* topic, byte* payload, unsigned int length);
-void blinkLED(unsigned int times, unsigned int duration);
+void blinkLED(uint8_t times, uint16_t duration);
 void publishSensorData(int sensorState);
 
 void setup() {
@@ -30,7 +30,10 @@ void setup() {
   pinMode(SENSOR_PIN, INPUT_PULLUP);
   pinMode(LED_PIN, OUTPUT);
 
+  WiFi.mode(WIFI_STA);
+  WiFi.setHostname("ESP32_Sensor");
   macAddress = WiFi.macAddress();
+
   connectWiFi();
   mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
   mqttClient.setCallback(mqttCallback);
@@ -49,28 +52,30 @@ void loop() {
     publishSensorData(sensorState);
     previousSensorState = sensorState;
   }
+
+  delay(30000);
 }
 
 void connectWiFi() {
-  // WiFi.begin(SSID, WIFI_PASSWORD);
-  WiFi.begin(SSID);
-  Serial.print("Connecting to WiFi ");
+  WiFi.disconnect();
+  WiFi.begin(SSID, WIFI_PASSWORD);
+  Serial.print(F("Connecting to WiFi "));
   Serial.println(SSID);
 
   uint8_t retries = 0;
   while (WiFi.status() != WL_CONNECTED && retries < 10) {
     blinkLED(2, 200);
     delay(1000);
-    Serial.print(".");
+    Serial.print(F("."));
     retries++;
   }
 
   if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("\nWiFi connected");
-    Serial.print("IP address: ");
+    Serial.println(F("\nWiFi connected"));
+    Serial.print(F("IP address: "));
     Serial.println(WiFi.localIP());
   } else {
-    Serial.println("\nFailed to connect to WiFi");
+    Serial.println(F("\nFailed to connect to WiFi"));
     ESP.restart();
   }
 }
@@ -81,52 +86,51 @@ void connectMQTTServer() {
       connectWiFi();
     }
 
-    Serial.print("Attempting MQTT connection...");
+    Serial.print(F("Attempting MQTT connection..."));
     String clientId = "ESP32_" + String(random(0xffff), HEX);
 
     if (mqttClient.connect(clientId.c_str())) {
-      Serial.println("connected");
+      Serial.println(F("connected"));
       mqttClient.subscribe("esp32/sensor_check");
       mqttClient.subscribe("esp32/sleep");
     } else {
-      Serial.print("failed, rc=");
+      Serial.print(F("failed, rc="));
       Serial.print(mqttClient.state());
-      Serial.println(" trying again in 5 seconds");
-      blinkLED(3, 200);
+      Serial.println(F(" trying again in 5 seconds"));
+      blinkLED(1, 200);
       delay(5000);
     }
   }
 }
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
-    Serial.print("Message arrived [");
-    Serial.print(topic);
-    Serial.print("] ");
+  Serial.print(F("Message arrived ["));
+  Serial.print(topic);
+  Serial.print(F("] "));
 
-    String messageTemp;
-    for (unsigned int i = 0; i < length; i++) {
-        Serial.print((char)payload[i]);
-        messageTemp += (char)payload[i];
-    }
-    Serial.println();
+  char messageTemp[length + 1];
+  memcpy(messageTemp, payload, length);
+  messageTemp[length] = '\0';
+  Serial.println(messageTemp);
 
-    if (String(topic) == "esp32/sensor_check") {
-        String pongTopic = "esp32/sensor_check/" + macAddress;
-        mqttClient.publish(pongTopic.c_str(), "pong");
-    }
+  if (String(topic) == "esp32/sensor_check") {
+    String pongTopic = "esp32/sensor_check/" + macAddress;
+    mqttClient.publish(pongTopic.c_str(), "pong");
+  }
 
-    if (String(topic) == "esp32/sleep") {
-        String pongTopic = "esp32/sleep/" + macAddress;
-        mqttClient.publish(pongTopic.c_str(), "slept");
-        Serial.println("Sleep for: " + messageTemp);
-        
-        long sleepDuration = messageTemp.toInt();
-        ESP.deepSleep(sleepDuration * 1e6);
-    }
+  if (String(topic) == "esp32/sleep") {
+    String pongTopic = "esp32/sleep/" + macAddress;
+    mqttClient.publish(pongTopic.c_str(), "slept");
+    Serial.print(F("Sleep for: "));
+    Serial.println(messageTemp);
+
+    long sleepDuration = atol(messageTemp);
+    ESP.deepSleep(sleepDuration * 1e6);
+  }
 }
 
-void blinkLED(unsigned int times, unsigned int duration) {
-  for (unsigned int i = 0; i < times; i++) {
+void blinkLED(uint8_t times, uint16_t duration) {
+  for (uint8_t i = 0; i < times; i++) {
     digitalWrite(LED_PIN, HIGH);
     delay(duration);
     digitalWrite(LED_PIN, LOW);
@@ -141,15 +145,15 @@ void publishSensorData(int sensorState) {
   doc["mac_address"] = macAddress;
 
   if (sensorState == LOW) {
-    Serial.println("Sensor triggered!");
+    Serial.println(F("Sensor triggered!"));
     doc["event"] = "triggered";
   } else {
-    Serial.println("Sensor released!");
+    Serial.println(F("Sensor released!"));
     doc["event"] = "released";
   }
 
   char buffer[256];
-  size_t n = serializeJson(doc, buffer);
+  size_t n = serializeJson(doc, buffer, sizeof(buffer));
   String dataTopic = "esp32/sensor_data/" + macAddress;
   mqttClient.publish(dataTopic.c_str(), buffer, n);
 }
